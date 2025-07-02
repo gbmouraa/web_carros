a6ed152d12e193359964122bca5a0e684811eaa8
gbmouraa <gabrielmoura-cma@hotmail.com>
2025-07-02 20:36:09 -0300

    feat: implement car listing with dynamic image loading and details display

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,62 @@
+import { useState, useEffect } from "react";
+import { collection, query, getDocs, orderBy } from "firebase/firestore";
+import { db } from "../services/firebase-connection";
 import { Container } from "../components/container";
+import { Link } from "react-router-dom";
+
+interface CarProps {
+  id: string;
+  name: string;
+  year: string;
+  uid: string;
+  price: string | number;
+  city: string;
+  km: string;
+  images: CarImageProps[];
+}
+
+interface CarImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
 
 export const Home = () => {
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const [loadImages, setLoadImages] = useState<string[]>([]);
+
+  useEffect(() => {
+    const loadCars = () => {
+      const carsRef = collection(db, "cars");
+      const queryRef = query(carsRef, orderBy("created", "desc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        const carList = [] as CarProps[];
+
+        snapshot.forEach((doc) => {
+          carList.push({
+            id: doc.id,
+            name: doc.data().name,
+            year: doc.data().year,
+            km: doc.data().km,
+            city: doc.data().city,
+            price: doc.data().price,
+            images: doc.data().images,
+            uid: doc.data().uid,
+          });
+        });
+
+        setCars(carList);
+      });
+    };
+
+    loadCars();
+  }, []);
+
+  const handleLoadImage = (id: string) => {
+    setLoadImages((prev) => [...prev, id]);
+  };
+
   return (
     <Container>
       <section className="mx-auto flex w-full max-w-3xl items-center justify-center gap-2 rounded-lg bg-white p-4">
@@ -17,26 +73,40 @@ export const Home = () => {
         Carros novos e usados em todo o Brasil
       </h1>
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <section className="w-full rounded-lg bg-white">
-          <img
-            className="mb-2 max-h-72 w-full rounded-lg transition-transform hover:scale-105"
-            src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2025/202505/20250516/toyota-hilux-sw4-2.8-d4d-turbo-diesel-srx-4x4-automatico-wmimagem00545091325.jpg?s=fill&w=552&h=414&q=60"
-            alt="Carro"
-          />
-          <p className="mt-1 mb-2 px-2 font-bold">SW4</p>
-          <div className="flex flex-col px-2">
-            <span className="mb-6 text-zinc-700">
-              Ano: 2022/20222 | 2.000 km
-            </span>
-            <strong className="text-xl font-medium text-black">
-              Preço: R$300.000,00
-            </strong>
-          </div>
-          <div className="my-2 h-px w-full bg-slate-200" />
-          <div className="px-2 pb-2">
-            <span className="text-zinc-700">Campo Grande - MS</span>
-          </div>
-        </section>
+        {cars.map((car) => (
+          <Link key={car.id} to={`/car-detail/${car.id}`}>
+            <section className="w-full rounded-lg bg-white">
+              <div
+                className="h-72 w-full rounded-lg bg-slate-200"
+                style={{
+                  display: loadImages.includes(car.id) ? "none" : "block",
+                }}
+              ></div>
+              <img
+                className="mb-2 max-h-72 w-full rounded-lg transition-transform hover:scale-105"
+                src={car.images[0].url}
+                alt="Carro"
+                onLoad={() => handleLoadImage(car.id)}
+                style={{
+                  display: loadImages.includes(car.id) ? "block" : "none",
+                }}
+              />
+              <p className="mt-1 mb-2 px-2 font-bold">{car.name}</p>
+              <div className="flex flex-col px-2">
+                <span className="mb-6 text-zinc-700">
+                  Ano: {car.year} | {car.km} km
+                </span>
+                <strong className="text-xl font-medium text-black">
+                  Preço: R$ {car.price}
+                </strong>
+              </div>
+              <div className="my-2 h-px w-full bg-slate-200" />
+              <div className="px-2 pb-2">
+                <span className="text-zinc-700">{car.city}</span>
+              </div>
+            </section>
+          </Link>
+        ))}
       </main>
     </Container>
   );
